6baca7dfc901831e1b8dd6e1860711fd24eda5e0
Hyeeun Mok <hyeeuni.mok@gmail.com>
2020-11-26 23:09:52 -0500

    deleted flask-secrets folder

---
+++ send-email/main.py
@@ -0,0 +1,24 @@
+import smtplib
+
+# Replace to your email and password
+my_email = "xxxxx.cs@gmail.com"
+password = "xxxx"
+
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="slothship@naver.com",
+        msg="Subject:Hello\n\nThis is the body of my email.")
+
+
+# import datetime as dt
+#
+# now = dt.datetime.now()
+# year = now.year
+# if year == 2020:
+#     print("Wear a face mask!")
+#
+# date_of_birth = dt.datetime(year=1995, month=12, day=15, hour=14)
+# print(date_of_birth)
