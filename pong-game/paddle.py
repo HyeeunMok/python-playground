b4eba8c623197da0ca9bb08c9333326c66e72ac6
Hyeeun Mok <hyeeuni.mok@gmail.com>
2020-12-10 22:43:19 -0500

    add right paddle

---
+++ pong-game/paddle.py
@@ -0,0 +1,14 @@
+# from turtle import Turtle
+#
+#
+# class Paddle:
+#
+#     def __init__(self):
+#         self.create_paddle()
+#
+#     def create_paddle(self):
+#         paddle = Turtle("square")
+#         paddle.color("white")
+#         paddle.shapesize(stretch_wid=5, stretch_len=1)
+#         paddle.penup()
+#         paddle.goto(350, 0)
