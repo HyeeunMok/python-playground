318fe9e2333a9f649e3119dd2a85b92b37a70f45
Hyeeun Mok <hyeeuni.mok@gmail.com>
2020-12-19 16:06:38 -0500

    finishing up us states game

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -7,41 +7,31 @@ screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-word = Turtle()
-word.hideturtle()
-style = ('Courier', 12, 'normal')
-game_over_style = ('Courier', 18, 'normal')
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+STYLE = ('Courier', 12, 'normal')
 
 data = pandas.read_csv("50_states.csv")
 state_list = data.state.to_list()
-state_dict = data.to_dict()
+guessed_states = []
 
-correct_answers = []
-correct_answers_count = 0
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        for state in guessed_states:
+            state_list.remove(state)
+        pandas.DataFrame(state_list).to_csv("states_to_learn.csv")
+        break
 
-game_is_on = True
-while game_is_on:
     if answer_state in state_list:
-        state_x_cor = int(data[data.state == answer_state].x)
-        state_y_cor = int(data[data.state == answer_state].y)
-        word.penup()
-        word.goto(state_x_cor, state_y_cor)
-        word.pendown()
-        word.write(f"{answer_state}", font=style, align="center")
-        correct_answers.append(answer_state)
-        correct_answers_count += 1
-        answer_state = screen.textinput(title=f"{correct_answers_count}/50 States Correct", prompt="What's another state's name?").title()
-        print(correct_answers)
-    else:
-        print("Not there")
-        word.penup()
-        word.goto(0, 0)
-        word.pendown()
-        word.write("Game Over!", font=game_over_style, align="center")
-        game_is_on = False
+        guessed_states.append(answer_state)
+        t = Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.pendown()
+        t.write(f"{answer_state}", font=STYLE, align="center")
 
-screen.exitonclick()
 
 
 # def get_mouse_click_coor(x, y):
