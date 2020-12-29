e63cd2957085f202da9bb18e0f1beb5f0dfb8f7a
Hyeeun Mok <hyeeuni.mok@gmail.com>
2020-12-29 18:40:49 -0500

    finished flight deals finder app

--- flight-deals/data_manager.py
+++ flight-deals/data_manager.py
@@ -14,10 +14,21 @@ class DataManager:
     def get_destination_data(self):
         response = requests.get(url=SHEETY_PRICES_ENDPOINT)
         data = response.json()
+        # pprint(data)
         self.destination_data = data["prices"]
-        pprint(self.destination_data)
+        # pprint(self.destination_data)
         return self.destination_data
 
-
+    # In the DataManager Class make a PUT request and use the row id  from sheet_data
+    # to update the Google Sheet with the IATA codes.
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}", json=new_data)
+            print(response.text)
 
 
--- flight-deals/notification_manager.py
+++ flight-deals/notification_manager.py
@@ -1,3 +1,23 @@
+from twilio.rest import Client
+import config
+
+# Twilio
+ACCOUNT_SID = config.twilio_account_sid
+AUTH_TOKEN = config.twilio_auth_token
+TWILIO_VIRTUAL_NUMBER = config.twilio_virtual_num
+TWILIO_VERIFIED_NUMBER = config.twilio_verified_num
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    # This class is responsible for sending notifications with the deal flight details.
+    def __init__(self):
+        self.client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER
+        )
+
+        print(message.status)
